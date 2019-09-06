9d375cd099152b45e00ed4b9bc281761fbc6391d
webdev-ninja <slark0828@gmail.com>
2019-09-07 02:12:28 +1000

    init project

---
+++ src/Slider.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <tiny-slider
+      :mouse-drag="true"
+      :loop="true"
+      :autoplay="true"
+      :autoplay-button-output="false"
+      :speed="500"
+      :controls="false"
+      items="3"
+      gutter="20"
+    >
+      <img alt="Slider 1" src="./assets/map-gallery-img-1.jpg" />      
+      <img alt="Slider 1" src="./assets/video-img-1-1100x550.jpg" />
+      <img alt="Slider 2" src="./assets/map-gallery-img-2.jpg" />
+      <img alt="Slider 1" src="./assets/video-img-2-1100x550.jpg" />
+      <img alt="Slider 3" src="./assets/map-gallery-img-3.jpg" />
+      <img alt="Slider 1" src="./assets/video-img-3-1100x550.jpg" />
+      <img alt="Slider 4" src="./assets/map-gallery-img-4.jpg"  />
+    </tiny-slider>    
+  </div>
+</template>
+
+<script>
+import VueTinySlider from "vue-tiny-slider";
+
+export default {
+  name: "app",
+  components: {
+    "tiny-slider": VueTinySlider
+  }
+};
+</script>
+
+<style>
+  #tns1 > .tns-item {
+    padding-right: 0px !important;
+    cursor: pointer;
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import Slider from './Slider.vue'
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(Slider),
+}).$mount('#app')
